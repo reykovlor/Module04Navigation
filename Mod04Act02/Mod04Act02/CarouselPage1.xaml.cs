d060196d2ba3006f65b98c7ac480d14235f8148b
kovlor13 <127574325+kovlor13@users.noreply.github.com>
2024-04-10 01:13:15 +0800

    Add project files.

---
+++ Mod04Act02/Mod04Act02/CarouselPage1.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mod04Act02
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CarouselPage1 : CarouselPage
+    {
+        public CarouselPage1()
+        {
+            InitializeComponent();
+        }
+
+        private async void NavigateToNextPage(object sender, EventArgs e)
+        {
+          
+            if (CurrentPage != null)
+            {
+                var nextPageIndex = Children.IndexOf(CurrentPage) + 1;
+                if (nextPageIndex < Children.Count)
+                    await CurrentPage.Navigation.PushAsync(Children[nextPageIndex]);
+            }
+        }
+
+        private async void NavigateBack(object sender, EventArgs e)
+        {
+          
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ Mod04Act02/Mod04Act02/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace Mod04Act02
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void GoToCarouselPage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CarouselPage1());
+        }
+
+        private async void GoToTabbedPage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TabbedPage1());
+        }
+    }
+}
